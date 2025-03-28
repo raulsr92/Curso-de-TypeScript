37e3cae3371da7dc80020728ef406efe0736e743
Raul Sanchez R. <raul.sr92@gmail.com>
2025-03-28 00:44:45 -0500

    Clase 3: tipos de datos primitivos

---
+++ variables_base/main.js
@@ -0,0 +1,10 @@
+var nombre = "Raul";
+var edad = 33;
+var esDesarrollador = true;
+//Tipo de variable que no estoy seguro si será de tipo cadena o indefinida
+var mes;
+var variableNull = null;
+console.log("Mes inicial: ".concat(mes));
+mes = "Febrero";
+console.log("Mes inicial: ".concat(mes));
+console.log("Hola, mi nombre es ".concat(nombre, ", nac\u00ED un 10 de ").concat(mes, ", ahora tengo ").concat(edad, ". \u00BFSoy desarrollador?: ").concat(esDesarrollador));
---
+++ variables_base/main.ts
@@ -0,0 +1,21 @@
+
+let nombre: string = "Raul";
+
+let edad: number = 33;
+
+let esDesarrollador: boolean = true;
+
+//Tipo de variable que no estoy seguro si será de tipo cadena o indefinida
+let mes: string | undefined;
+
+let variableNull: null = null;
+
+
+console.log(`Mes inicial: ${mes}`)
+
+mes = "Febrero"
+
+console.log(`Mes inicial: ${mes}`)
+
+
+console.log(`Hola, mi nombre es ${nombre}, nací un 10 de ${mes}, ahora tengo ${edad}. ¿Soy desarrollador?: ${esDesarrollador}`)
